cedeec82f95d3668697434a3f6e7753b2422746d
novafacing <rowanbhart@gmail.com>
2022-02-09 11:58:30 -1000

    Try to actually add cat

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -27,16 +27,43 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
+		// loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		// loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
-		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-		});
-	});
+    L2Dwidget.init({
+      "pluginRootPath": "live2d-widget/",
+      "pluginJsPath": "/",
+      "pluginModelPath": "assets/",
+      "tagMode": false,
+      "debug": true,
+      "tagMode":false,
+      "debug":false,
+      "model": {
+        "scale":1,
+        "hHeadPos":0.5,
+        "vHeadPos":0.618,
+        "jsonPath":"/live2d-widget/assets/tororo/tororo.model3.json"
+      },
+      "display": {
+        "superSample":2,
+        "position":"left",
+        "width":150,
+        "height":300,
+        "hOffset":20,
+        "vOffset":-90
+      },
+      "mobile": {
+        "show":true,
+        "scale":1
+      },
+      "react": {
+        "opacityDefault":0.3,
+        "opacityOnHover":0.3,
+        "opacity":0.95},
+      "log":false
+    });
+  });
 }
 // initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
 // API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
